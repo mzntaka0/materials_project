f00b9f5be7741d00a76823899ff6f7ea5a65646b
Takao Mizuno <takao.mizuno110@gmail.com>
2017-12-21 15:35:28 +0900

    added detailed conversion efficiency

--- controllers/integral.py
+++ controllers/integral.py
@@ -10,13 +10,13 @@ import matplotlib.pyplot as plt
 
 def trapezoidal(x_list, y_list):
     S = 0.0
-    for i in range(len(x_list) - 1):
-        S += abs(y_list[i+1] + y_list[i]) * abs(x_list[i+1] - x_list[i]) / 2.0
+    for i in range(len(y_list) - 1):
+        S += 0.5 * abs(y_list[i+1] + y_list[i]) * abs(x_list[i+1] - x_list[i])
     return S
 
 
 if __name__ == '__main__':
-    x = np.linspace(0, 2*np.pi, 100)
+    x = np.linspace(0, 4*np.pi, 5000)
     y = np.sin(x)
 
     plt.scatter(x, y)
--- tasks/calc_abs_intensity.py
+++ tasks/calc_abs_intensity.py
@@ -7,7 +7,11 @@ import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from bpdb import set_trace
+from scipy import optimize, log
+try:
+    from bpdb import set_trace
+except ImportError:
+    from pdb import set_trace
 from scipy import integrate
 from scipy.constants import Planck, c, k, e
 
@@ -16,31 +20,18 @@ from controllers.luminous import Luminous
 from controllers.integral import trapezoidal
 
 
-
 class ConversionEfficiency:
     J = 1.60217646e-19
 
     def __init__(self, wavelength_list, photon_num_list):
         self.wavelength_list = wavelength_list * 1e-9
         self.photon_num_list = photon_num_list
+        print('wavelength_list: {}'.format(len(self.wavelength_list)))
         self.Pin = self.Pin()
-        self.band_gap_list = np.arange(0.01, 3.01, 0.01)
+        self.band_gap_list = np.linspace(0.01, 3.01, 1800)
         self.Tc = 300  # [K]
         self.Vc = k * self.Tc / e
         self.fc = 1.0
-        self.Fs = np.array([self.num_of_photon(band_gap) for band_gap in self.band_gap_list])
-        self.Ish = e * self.Fs
-        plt.scatter(self.band_gap_list, self.Ish)
-        plt.title('Ish - bandgap')
-        plt.xlabel('Band gap [eV]')
-        plt.ylabel('Short currency [A]')
-        plt.show()
-        #self.band_gap_frequency_list = self.band_gap_list / Planck
-        #print('band_gap_frequency_list: {}'.format(self.band_gap_frequency_list))
-        #print('band_gap_wavelength_list: {}'.format(self.band_gap_wavelength_list))
-        #self.Ts = 1.0  # assume
-        #self.xc = self.Tc / self.Ts
-
 
 
     ##############################
@@ -61,45 +52,44 @@ class ConversionEfficiency:
     # 2. nu
     ##############################
     def nu(self, band_gap):
+        """
+        nu = Vop / Vg = Vc*log(fc*num_of_photon(band_gap)/2*pi*Pflux(band_gap))
+        """
         Vg = band_gap * self.J / e
-        return self.Vop(band_gap * self.J) / Vg
+        Vop = self._Vop(band_gap)
+        return Vop / Vg
 
     def Vop(self, band_gap):
-        return self.Vc * np.log((self.fc * self.num_of_photon(band_gap)) / self.Fc0(band_gap))
+        return self.Vc * np.log((self.fc * self.num_of_photon(band_gap)) / (2.0 * np.pi * self.Pflux(band_gap)))
 
-    def num_of_photon(self, band_gap):
-        wavelengthEg = Planck * c / (band_gap * self.J)
-        return trapezoidal(self.wavelength_list, np.where(self.wavelength_list <= wavelengthEg, self.photon_num_list, 0))
+    def _Vop(self, band_gap):
+        return self.Vc * np.log(self.fc * (self.num_of_photon(band_gap) / self.Fc0(band_gap)) + 1)
+        
 
     def Fc0(self, band_gap):
-        return 2*np.pi*self.Pflux(band_gap)
+        return self.Pflux(band_gap) * 2.0 * np.pi
 
-    def Pflux(self, band_gap):
-        """determined by the temperature of solar cell"""
-        nug = (band_gap * self.J) / Planck
-        nu_list = c / self.wavelength_list
-        target_nu_list = np.where(nu_list > nug, nu_list, 0.0)
-        pflux = np.vectorize(self._integrated_f)(target_nu_list)
-        return trapezoidal(nu_list, pflux)
 
-    def _integrated_f(self, nu):
+    def num_of_photon(self, band_gap):
+        wavelengthEg = Planck * c / (band_gap * self.J)  # self.J point
+        return trapezoidal(self.wavelength_list, np.where(self.wavelength_list <= wavelengthEg, self.photon_num_list, 0))
+        #return trapezoidal(self.wavelength_list, np.where(self.wavelength_list <= wavelengthEg, self.photon_num_list, 0))
+
+    def Pflux(self, band_gap, T=300):
+        """determined by the temperature50 of solar cell"""
+        nug = band_gap * self.J / Planck  # self.J point
+        y = lambda x: (2.0/c**2)*(x**2)/(np.exp((Planck*x)/(k*self.Tc)) - 1.0)
+        S = integrate.quad(y, nug, 1e16) 
+        return S[0]
+
+    def _integrated_func(self, nu, T=300):
         if nu == 0.0:
             return 0.0
         else:
-            return (2/c**2)*((nu**2)/(np.exp(Planck*nu/(k*self.Tc)) - 1.0))
+            return (2/(c**2))*(nu**2)/(np.exp((Planck*nu)/(k*self.Tc)) - 1.0)
         
-        #return trapezoidal(nu_list, np.where(nu_list > nug, self._integrated_f(nu), 0))
-        #print('num_of_zero: {}'.format(len(np.where(target_nu_list)[0])))
-        #power = Planck*target_nu_list / (k*self.Tc)
-        #denom = np.exp(power) - 1
-        #print('sum_denom: {}'.format(np.sum(denom)))
-        #numer = target_nu_list**2
-        #print('sum_numer: {}'.format(np.sum(numer)))
-        #integrated = numer/denom
-        #integrated[np.isnan(integrated)] = 0.0
-        #return (2/c**2) * trapezoidal(nu_list, integrated)
-
-
+    def _integrated(self, nu, T=300):
+        return (2/(c**2))*(nu**2)/(np.exp((Planck*nu)/(k*self.Tc)) - 1.0)
 
     ##############################
     # 3. FF
@@ -109,10 +99,19 @@ class ConversionEfficiency:
         return ((Vmax/self.Vc)**2) / ((1 + (Vmax/self.Vc) - np.exp(Vmax/self.Vc)) * (Vop / self.Vc))
 
     def FF(self):
-        return (I(Vmax) * Vmax) / (Ish * Vop)
+        return (I(Vmax) * Vmax) / (self.Ish * self.Vop)
 
-    def FF_zm(self, zm):
+    def FF_zm(self, band_gap):
+        zm = self.solve_zm(band_gap)
         return zm**2 / ((1 + zm - np.exp(-zm)) * (zm + np.log(1 + zm)))
+
+    def solve_zm(self, band_gap):
+        return optimize.fsolve(self.formula_zm, 0, args=(band_gap))
+
+    def formula_zm(self, zm, band_gap):
+        return zm + np.log(1 + zm) - (self._Vop(band_gap) / self.Vc)
+
+
 
 
 def show(y):
@@ -164,16 +163,36 @@ if __name__ == '__main__':
 
     graph2coords.num_photons_list = \
             (graph2coords.intensity_list * alpha) / (Planck * c / graph2coords.wavelength_list)
-    graph2coords.show_graph(graph2coords.wavelength_list, graph2coords.num_photons_list)
+
+    if imshow:
+        graph2coords.show_graph(graph2coords.wavelength_list, graph2coords.num_photons_list)
+
+        plt.scatter(graph2coords.wavelength_list, graph2coords.intensity_list*alpha)
+        plt.title('peak spectral intensity')
+        plt.xlabel('wavelength[nm]')
+        plt.ylabel('peak spectral intensity[W*m^-2*nm^1]')
+        plt.show()
 
     conversion_efficiency = ConversionEfficiency(graph2coords.wavelength_list, graph2coords.num_photons_list)
     print('Pin: {}'.format(conversion_efficiency.Pin))
     ideal = conversion_efficiency.u()
     print('max efficiency: {}'.format(conversion_efficiency.band_gap_list[np.argmax(ideal)]))
-    plt.scatter(conversion_efficiency.band_gap_list, ideal)
-    plt.xlabel('band gap [eV]')
-    plt.ylabel('efficiency [%]')
-    plt.show()
+    if imshow:
+        plt.scatter(conversion_efficiency.band_gap_list, ideal)
+        plt.xlabel('band gap [eV]')
+        plt.ylabel('efficiency [%]')
+        plt.show()
     nu = np.vectorize(conversion_efficiency.nu)(conversion_efficiency.band_gap_list)
-    plt.scatter(conversion_efficiency.band_gap_list, nu*ideal)
+    FF = np.vectorize(conversion_efficiency.FF_zm)(conversion_efficiency.band_gap_list)
+    ita = nu*ideal*FF
+    ita_max = np.argmax(ita)
+    print('Eg: {}, ita: {}'.format(conversion_efficiency.band_gap_list[ita_max], ita[ita_max]))
+    plt.scatter(conversion_efficiency.band_gap_list, ita)
+    plt.title('conversion efficiency considering with detailed balance + FF')
+    plt.xlabel('Band gap [eV]')
+    plt.ylabel('conversion efficiency [%]')
+    plt.savefig('conversion efficiency')
     plt.show()
+    ############################################
+    # until here OK
+    ############################################
